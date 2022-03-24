cb358bb9fcbd41662a896fd93e54ed15948a8c24
Alessandro Lorenzi <lorenzi.alessandro19@gmail.com>
2022-03-24 20:52:05 +0100

    esempio di utlizzo di shared memory segment (memoria condivisa per comunicazione tra pocessi)

--- TEO/IPC Esempi/p2.c
+++ TEO/IPC Esempi/p2.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
 
+// esempio di utlizzo shared memory segment
+// legato a p2.c
+
 int main()
 
 {
- key_t key;
- int   i,a;
-int  shm;
-char *addr;
-    struct shmid_ds buf;
+  key_t key;
+  int i, a;
+  int shm;
+  char *addr;
+  struct shmid_ds buf;
 
-    key = 8888;
-    shm = shmget(key,1000,S_IRUSR+S_IWUSR);
-    addr = shmat(shm,NULL,0);
-    strncat(addr, "P2 added this content to shared memory",  1000 - (strlen(addr)+1));
+  key = 8888;
+  shm = shmget(key, 1000, S_IRUSR + S_IWUSR);
+  addr = shmat(shm, NULL, 0);
+  strncat(addr, "P2 added this content to shared memory", 1000 - (strlen(addr) + 1));
 
-    printf("P2: identifier of the shared memory shm= %d\n", shm);
-    printf("P2: address of the shared memory addr= %d\n", *addr);
-    printf(" %s %d\n",  addr,shm); 
+  printf("P2: identifier of the shared memory shm= %d\n", shm);
+  printf("P2: address of the shared memory addr= %d\n", *addr);
+  printf(" %s %d\n", addr, shm);
 
-/*
-    shmdt(addr(;
-    shmctl(shm, IPC_RMID,0);
-*/
-  
- }
+  /*
+      shmdt(addr(;
+      shmctl(shm, IPC_RMID,0);
+  */
+}
