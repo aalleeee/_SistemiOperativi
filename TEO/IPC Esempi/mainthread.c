013de5f3d1cb085c90b44840166a23d5b4acc17a
Alessandro Lorenzi <lorenzi.alessandro19@gmail.com>
2022-03-24 18:53:52 +0100

    esempio di base utilizzo thread

--- TEO/IPC Esempi/mainthread.c
+++ TEO/IPC Esempi/mainthread.c
@@ -3,32 +3,33 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+// ESEMPIO DI BASE UTILIZZO DI THREAD
+
 void *tbody(void *arg)
- {
-    int j;
-
-     printf("ciao sono un thread, mi hanno appena creato\n");
-     *(int *)arg = 10;
-     sleep(5); /* faccio aspettare un pò il mio creatore, poi termino */
-     pthread_exit((int *)50); /* oppure return ((int *)50); */
-}
-
-
-main(int argc, char **argv)
 {
+  int j;
 
- int i;
- pthread_t mythread;
- void *result;
-
- i=1;
- printf("i=%d \n", i);
- printf("sono il primo thread, ora ne creo un altro \n");
- pthread_create(&mythread, NULL, tbody, (void *) &i);
- printf("ora aspetto la terminazione del thread che ho creato \n");
- pthread_join(mythread, &result);
-
- printf("Il thread creato ha assegnato %d ad i\n",i);
- printf("Il thread ha restituito %d \n", result);
+  printf("ciao sono un thread, mi hanno appena creato\n");
+  *(int *)arg = 10;
+  sleep(5);                /* faccio aspettare un pò il mio creatore, poi termino */
+  pthread_exit((int *)50); /* oppure return ((int *)50); */
 }
 
+int main(int argc, char **argv)
+{
+  int i;
+  pthread_t mythread;
+  void *result;
+
+  i = 1;
+  printf("i=%d \n", i);
+  printf("sono il primo thread, ora ne creo un altro \n");
+  pthread_create(&mythread, NULL, tbody, (void *)&i);
+  printf("ora aspetto la terminazione del thread che ho creato \n");
+  pthread_join(mythread, &result);
+
+  printf("Il thread creato ha assegnato %d ad i\n", i);
+  printf("Il thread ha restituito %d \n", result);
+
+  return 0;
+}
